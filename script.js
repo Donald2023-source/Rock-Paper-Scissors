ffd4f5f60f83bc4da0d142a287581d1ec896f059
Donald2023-source <donalddyusuf@gmail.com>
2025-01-21 20:08:56 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,111 @@
+let playerMove = '';
+let computerMove = '';
+let result = '';
+let score;
+
+
+
+if (localStorage.getItem('score')) {
+  score = JSON.parse(localStorage.getItem('score'));
+} else {
+  score = { Wins: 0, Loses: 0, Ties: 0 };
+}
+
+
+
+function selectComputerMove() {
+  let randomMove = Math.random();
+  if (randomMove <= 1 / 3) {
+    computerMove = 'Rock';
+  } else if (randomMove <= 2 / 3) {
+    computerMove = 'Paper';
+  } else {
+    computerMove = 'Scissors';
+  }
+
+}
+
+function assignScore() {
+  if (result === 'Win') {
+    score.Wins += 1;
+  } else if (result === 'Lose') {
+    score.Loses += 1;
+  } else if (result === 'Tie') {
+    score.Ties += 1;
+  }
+
+  localStorage.setItem('score', JSON.stringify(score));
+
+  console.log(score);
+}
+
+function updateScore() {
+  let score_tag = document.getElementById('score')
+  score_tag.textContent = score.Wins
+}
+
+updateScore()
+
+
+function makeMove(playerMove) {
+  selectComputerMove();
+  console.log(`You chose ${playerMove} and Computer chose ${computerMove}`);
+  
+  if (playerMove === computerMove) {
+    console.log('Tie');
+    result = 'Tie';
+  } else if (playerMove === 'Rock' && computerMove === 'Scissors') {
+    console.log('You win');
+    result = 'Win';
+  } else if (playerMove === 'Paper' && computerMove === 'Scissors') {
+    console.log('You Lose');
+    result = 'Lose';
+  } else if (playerMove === 'Scissors' && computerMove === 'Paper') {
+    console.log('You win');
+    result = 'Win';
+  } else if (playerMove === 'Scissors' && computerMove === 'Rock') {
+    console.log('You Lose');
+    result = 'Lose';
+  } else if (playerMove === 'Paper' && computerMove === 'Rock') {
+    console.log('You win');
+    result = 'Win';
+  } else if (playerMove === 'Rock' && computerMove === 'Paper') {
+    console.log('You Lose');
+    result = 'Lose';
+  }
+
+  assignScore();
+  updateScore()
+}
+
+
+console.log('Stored score:', score);
+
+function resetScore() {
+  localStorage.clear()
+  score = { Wins: 0, Loses: 0, Ties: 0 };
+  localStorage.setItem('score', JSON.stringify(score));
+}
+
+const leadboard = document.querySelector('.card')
+const overlay = document.querySelector('.overlay')
+
+
+function closeLeadboard() {
+  leadboard.classList.remove('show')
+  overlay.classList.remove('show')
+}
+
+function showLeadboard() {
+  leadboard.classList.add('show')
+  overlay.classList.add('show')
+
+  let leadboard_wins = document.querySelector('.wins');
+  let leadboard_Loses = document.querySelector('.loses')
+  let leadboard_Ties = document.querySelector('.ties')
+
+  leadboard_wins.textContent = score.Wins;
+  leadboard_Loses.textContent = score.Loses;
+  leadboard_Ties.textContent = score.Ties;
+  resetScore()
+}
